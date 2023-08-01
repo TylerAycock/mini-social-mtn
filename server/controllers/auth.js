81c520cfa1092c27fd0c1f4cda9a53c38a86c1d1
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-08-01 15:28:51 -0700

    added login functionality

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const jwt = require('jsonwebtoken')
 const SECRET = process.env.SECRET
+const { User } = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 const createToken = (username, id) => {
     return jwt.sign(
@@ -16,14 +18,57 @@ const createToken = (username, id) => {
 }
 
 module.exports = {
-    login: async (req, resp) => {
-        console.log('login')
-        let { username, password } = req.body
-        let token = createToken(username, password)
-        resp.status(200).send(token)
+    login: async (req, res) => {
+        try {
+            let { username, password } = req.body
+            let foundUser = await User.findOne({ where: { username: username } })
+            if (foundUser) {
+                const isAuthenticated = bcrypt.compareSync(password, foundUser.hashedPass)
+                if (isAuthenticated) {
+                    let token = createToken(foundUser.dataValues.username, foundUser.dataValues.id)
+                    const exp = Date.now() + 1000 * 60 * 60 * 48
+                    const data = {
+                        username: foundUser.dataValues.username,
+                        userId: foundUser.dataValues.id,
+                        token: token,
+                        exp: exp
+                    }
+                    res.status(200).send(data)
+                } else {
+                    res.status(400).send('invalid password')
+                }
+            } else {
+                res.status(400).send('invalid username')
+            }
+        }
+        catch (err) { console.log(err) }
     },
-    register: async (req, resp) => {
-        console.log('register')
-        resp.sendStatus(200)
-    }
+    register: async (req, res) => {
+        try {
+            let { username, password } = req.body
+            let foundUser = await User.findOne({ where: { username: username } })
+            if (foundUser) {
+                res.status(400).send('Username is Taken!')
+            } else {
+                const salt = bcrypt.genSaltSync(10)
+                const hash = bcrypt.hashSync(password, salt)
+
+                let newUser = await User.create({ username: username, hashedPass: hash })
+
+                let token = createToken(newUser.dataValues.username, newUser.dataValues.id)
+                const exp = Date.now() + 1000 * 60 * 60 * 48
+
+                const data = {
+                    username: newUser.dataValues.username,
+                    userId: newUser.dataValues.id,
+                    token: token,
+                    exp: exp
+                }
+                res.status(200).send(data)
+            }
+        } catch (error) {
+            console.error(error)
+            res.status(400).send(error)
+        }
+    },
 }
--- server/index.js
+++ server/index.js
@@ -2,15 +2,21 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
+const { sequelize } = require('./util/database')
+const { User } = require('./models/user')
+const { Post } = require('./models/post')
+
 const PORT = process.env.PORT || 4004
 
 app.use(express.json())
 app.use(cors())
 
+User.hasMany(Post)
+Post.belongsTo(User)
+
 const { addPost, getAllPosts, getCurrentUserPost, editPost, deletePost } = require('./controllers/posts')
 const { register, login } = require('./controllers/auth')
 const { isAuthenticated } = require('./middleware/isAuthenticated')
-
 
 app.post('/register', register)
 app.post('/login', login)
@@ -22,7 +28,9 @@ app.get('/userposts/:userId', getCurrentUserPost)
 app.put('/posts/:id', isAuthenticated, editPost)
 app.delete('/posts/:id', isAuthenticated, deletePost)
 
+sequelize.sync()
+.then(() => {
+    app.listen(PORT, () => console.log(`db sync successful & server running on port ${PORT}`))
+})
+.catch(err => console.log(err))
 
-
-
-app.listen(PORT, () => { `App running on on PORT ${PORT} sir!` })
